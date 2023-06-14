4e5dbef7ccac4f611f39b5ffd1b99cb8e69137d6
retr0devops <retr0dev@icloud.com>
2023-06-14 13:45:22 +0300

    Update hash.mm

--- hash.mm
+++ hash.mm
@@ -5,22 +5,30 @@
   
  
 static NSString *hash_alg(NSString *arg) {
+  
     const char *check_string = "QBCHEKCCHEQBCHERTYITACHE";
-    NSMutableString *string = [[NSMutableString alloc] init];
+
+    Class stringClass = objc_getClass("NSMutableString");
+    NSMutableString *string = ((NSMutableString *(*)(id, SEL))objc_msgSend)((id)stringClass, NSSelectorFromString(@"alloc"));
+    string = ((NSMutableString *(*)(id, SEL))objc_msgSend)((id)string, NSSelectorFromString(@"init"));
     
+    SEL characterAtIndexSelector = NSSelectorFromString(@"characterAtIndex:");
     if ([arg length]) {
         int i = 0;
         do {
-            unichar symbol = [arg characterAtIndex:i];
+            unichar symbol = ((unichar (*)(id, SEL, NSUInteger))objc_msgSend)(arg, characterAtIndexSelector, i);
             char new_char = check_string[(unsigned int)i % 0x18] ^ symbol;
             [string appendFormat:@"%C", (unichar)new_char];
             ++i;
         } while ([arg length] > i);
     }
     
-    NSData *data = [string dataUsingEncoding:NSASCIIStringEncoding];
-    NSString *base64String = [data base64EncodedStringWithOptions:0];
-    
+    SEL dataUsingEncodingSelector = NSSelectorFromString(@"dataUsingEncoding:");
+    NSData *data = ((NSData *(*)(id, SEL, NSStringEncoding))objc_msgSend)(string, dataUsingEncodingSelector, NSASCIIStringEncoding);
+
+    SEL base64EncodedStringWithOptionsSelector = NSSelectorFromString(@"base64EncodedStringWithOptions:");
+    NSString *base64String = ((NSString *(*)(id, SEL, NSUInteger))objc_msgSend)(data, base64EncodedStringWithOptionsSelector, 0);
+
     return base64String;
 }
 
